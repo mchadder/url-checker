8ec4b4cefc2e9d9fec1f59517ce7fe83027418e8
Martin Chadderton <chadders@oratechinfo.co.uk>
2016-04-22 11:15:10 +0100

    Specify the full URL in the file. This allows the same file to apply to multiple web applications. Just will rely on the find/replace functionality in the editor of choice

--- url-checker.sh
+++ url-checker.sh
@@ -2,21 +2,18 @@
 # Syntax: ./url-checker.sh <filename> <show_passes>[Y|N] <show_fails>[Y|N]
 if [ -f "$1" -a "$2" != "" -a "$3" != "" ]
 then
-  URL_ROOT="http://localhost:8000"
-
   while read p; do
+    # Allow # comment lines
     if [[ ${p:0:1} != "#" ]]
     then
       HTTP_CODE=`echo $p | cut -f 1 -d " "`
       URL=`echo $p | cut -f 2 -d " "`
 
-      FULL_URL=$URL_ROOT$URL
-
       # Have to use the dread eval() here since brace expansion occurs before 
       # parameter expansion. Be careful that the URL list in the file 
       # does not contain any arbitrary UNIX commands. Ideally, should validate that the output
       # is a valid URL before running it through eval().
-      for OUTPUT_URL in $(eval echo $FULL_URL);
+      for OUTPUT_URL in $(eval echo $URL);
       do 
         CURL=`curl -sL -w "%{http_code}" $OUTPUT_URL -o /dev/null`
 
